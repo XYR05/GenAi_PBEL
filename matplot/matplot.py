26045b8582a6498e9a8ea96ffd34ccf5a31fd7aa
codddeeerrr <pctarvis@gmail.com>
2026-06-25 09:02:41 +0530

    ordered files into folders

---
+++ matplot/matplot.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+# plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+# #plt.show()
+
+# x=np.array([1,2,3,4])
+# y=np.array([1,4,9,16])
+# plt.plot(x,y, '--o')
+# #plt.show()
+
+
+# y=np.array([1,4,9,16])
+# plt.plot(y, 'o:r')  
+# #plt.show()
+
+# x=np.array([1,2,3,4])
+# plt.plot(x,  label='x squared')
+# #plt.show()
+
+# # plt.plot(x)
+# # plt.plot(y)
+# # plt.show()
+
